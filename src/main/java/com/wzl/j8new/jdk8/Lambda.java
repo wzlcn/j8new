a0652b53ec962626a25b2c8b1de3f16923b031a2
wzl <15303789723@139.com>
2022-07-24 23:28:01 +0800

    回退测试

--- src/main/java/com/wzl/j8new/jdk8/Lambda.java
+++ src/main/java/com/wzl/j8new/jdk8/Lambda.java
@@ -22,6 +22,5 @@ public class Lambda {
         //flatMap降维
         List<String> strings = Arrays.asList("Hello", "World");
         strings.stream().map(x -> x.split("")).flatMap(Arrays::stream).forEach(y -> System.out.println(y));
-
     }
 }
